c764b35d47dec49168a21897590c81221e783615
Jitendra Prajapati <jpr@openerp.com>
2014-07-18 17:01:14 +0530

    [REM]hr_evaluation: remove commented code and remove code for related hr_evaluation.plan,hr_evaluation.plan.phase,hr_evaluation.interview

--- addons/hr_evaluation/report/hr_evaluation_report.py
+++ addons/hr_evaluation/report/hr_evaluation_report.py
@@ -34,7 +34,10 @@ class hr_evaluation_report(osv.Model):
         'deadline': fields.date("Deadline", readonly=True),
         'request_id': fields.many2one('survey.user_input', 'Request_id', readonly=True),
         'closed': fields.date("closed", readonly=True),
-        'plan_id': fields.many2one('hr_evaluation.plan', 'Plan', readonly=True),
+        'year': fields.char('Year', size=4, readonly=True),
+        'month': fields.selection([('01', 'January'), ('02', 'February'), ('03', 'March'), ('04', 'April'),
+            ('05', 'May'), ('06', 'June'), ('07', 'July'), ('08', 'August'), ('09', 'September'),
+            ('10', 'October'), ('11', 'November'), ('12', 'December')], 'Month', readonly=True),
         'employee_id': fields.many2one('hr.employee', "Employee", readonly=True),
         'rating': fields.selection([
             ('0', 'Significantly bellow expectations'),
@@ -55,45 +58,10 @@ class hr_evaluation_report(osv.Model):
     _order = 'create_date desc'
 
     _depends = {
-        'hr.evaluation.interview': ['evaluation_id', 'id', 'request_id'],
         'hr_evaluation.evaluation': [
-            'create_date', 'date', 'date_close', 'employee_id', 'plan_id',
-            'rating', 'state',
+            'create_date', 'interview_deadline', 'date_close', 'employee_id','state',
         ],
     }
-
-    def init(self, cr):
-        tools.drop_view_if_exists(cr, 'hr_evaluation_report')
-        cr.execute("""
-            create or replace view hr_evaluation_report as (
-                 select
-                     min(l.id) as id,
-                     date(s.create_date) as create_date,
-                     s.employee_id,
-                     l.request_id,
-                     s.plan_id,
-                     s.rating,
-                     s.date as deadline,
-                     s.date_close as closed,
-                     count(l.*) as nbr,
-                     s.state,
-                     avg(extract('epoch' from age(s.create_date,CURRENT_DATE)))/(3600*24) as  delay_date,
-                     avg(extract('epoch' from age(s.date,CURRENT_DATE)))/(3600*24) as overpass_delay
-                     from
-                 hr_evaluation_interview l
-                LEFT JOIN
-                     hr_evaluation_evaluation s on (s.id=l.evaluation_id)
-                 GROUP BY
-                     s.create_date,
-                     s.state,
-                     s.employee_id,
-                     s.date,
-                     s.date_close,
-                     l.request_id,
-                     s.rating,
-                     s.plan_id
-            )
-        """)
 
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
