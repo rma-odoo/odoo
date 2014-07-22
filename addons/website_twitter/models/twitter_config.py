435b7fe7c5a767d8df02716ce5c3caa00e98e931
Jay Patel <jpa@openerp.com>
2014-07-22 18:05:39 +0530

    [IMP] twitter authentication error popup with proper description base on twitter error code and responses

--- addons/website_twitter/models/twitter_config.py
+++ addons/website_twitter/models/twitter_config.py
@@ -2,6 +2,7 @@ import logging
 
 from openerp.osv import fields, osv
 from openerp.tools.translate import _
+from urllib2 import Request, URLError, HTTPError
 
 _logger = logging.getLogger(__name__)
 
@@ -31,9 +32,25 @@ class twitter_config_settings(osv.osv_memory):
         website_config = self.browse(cr, uid, config_id, context=context)
         try:
             website_obj.fetch_favorite_tweets(cr, uid, [website_config.website_id.id], context=context)
-        except Exception:
-            _logger.warning('Failed to verify twitter API authorization', exc_info=True)
-            raise osv.except_osv(_('Twitter authorization error!'), _('Please double-check your Twitter API Key and Secret'))
+        except HTTPError, e:
+            _logger.warning("%s - %s" % (e.code or None, e.reason or None), exc_info=True)
+            if e.code == 401:
+                raise osv.except_osv(_("%s - %s" % (e.code, e.reason)), _('Authentication credentials are missing or incorrect (Screen name tweet are protected!)'))
+            elif e.code == 403:
+                raise osv.except_osv(_("%s - %s" % (e.code, e.reason)), _('The request is understood, but it has been not allowed to access (Check API or secret key).'))
+            elif e.code == 429:
+                raise osv.except_osv(_("%s - %s" % (e.code, e.reason)), _('Request are overloaded in short times (reach at maximum server limit).'))
+            elif e.code == 500 or 502 or 503 or 504:
+                raise osv.except_osv(_("%s - %s" % (e.code, e.reason)), _('Twitter server is down or being upgraded.'))
+            else:
+                _logger.warning("HTTP Error Something is misconfigured!", exc_info=True)
+                raise osv.except_osv(_("%s - %s" % (e.code or None, e.reason or None)), _('HTTP Error Something is misconfigured!'))
+        except URLError, e:
+            _logger.warning("We failed to reach a twitter server.", exc_info=True)
+            raise osv.except_osv(_("Internet connection refused"), _('We failed to reach a twitter server.'))
+        except Exception, e:
+            _logger.warning("Failed to verify twitter API authorization", exc_info=True)
+            raise osv.except_osv(_("Twitter authorization error!"), _("Please double-check your Twitter API Key and Secret!"))
 
     def create(self, cr, uid, vals, context=None):
         res_id = super(twitter_config_settings, self).create(cr, uid, vals, context=context)
