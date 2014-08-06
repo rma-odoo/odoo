fe93ef55a5ad2664c91cee50f83cce500f05ef7c
Vidhin Mehta <vme@openerp.com>
2014-08-06 14:13:25 +0530

    [IMP]forgot to add file.

---
+++ addons/website_url/controller/main.py
@@ -0,0 +1,23 @@
+from openerp.addons.web import http
+from openerp.tools.translate import _
+from openerp.http import request
+from openerp import SUPERUSER_ID
+import datetime
+import werkzeug
+
+class Website_Url(http.Controller):
+    @http.route('/r/<string:code>' ,type='http', auth="none", website=True)
+    def full_url_redirect(self, code, **post):
+        record = request.registry.get('website.alias').search_read(request.cr, SUPERUSER_ID, [('code', '=', code)], ['url'], limit=1, context= request.context)
+        rec = record and record[0] or False
+        if rec:
+            country_id = request.registry.get('res.country').search(request.cr, SUPERUSER_ID, [('code','=',request.session.geoip.get('country_code'))])
+            vals = {
+                    'alias_id':rec.get('id'),
+                    'create_id':datetime.datetime.now().date(),
+                    'ip':request.httprequest.remote_addr,
+                    'country_id': country_id and country_id[0] or False
+                    }
+            request.registry.get('website.alias.click').create(request.cr, SUPERUSER_ID, vals, context=request.context)
+        return werkzeug.utils.redirect(rec.get('url'))
+
