80b7c3205be3da4cf6f257effd592013f1cffb77
ptr <ptr@openerp.com>
2014-08-11 14:39:34 +0530

    [IMP] project : move all fields planned_hours, effective_hours,         total_hours, progress_rate and etc for project.project and project.task in project     [IMP] project & project_timesheet : project_task_work remove object and improve         everything related project.project, project.task     [IMP] project_issue : move fields progress for project.issue in project_issue_sheet with all dependancy.     [IMP] project_issue_sheet : add dependancy module project_timesheet.     [IMP] analytic : set user_id defaults value fix.

--- addons/project_issue/project_issue.py
+++ addons/project_issue/project_issue.py
@@ -199,16 +199,6 @@ class project_issue(osv.Model):
 
         return res
 
-    def _hours_get(self, cr, uid, ids, field_names, args, context=None):
-        task_pool = self.pool.get('project.task')
-        res = {}
-        for issue in self.browse(cr, uid, ids, context=context):
-            progress = 0.0
-            if issue.task_id:
-                progress = task_pool._hours_get(cr, uid, [issue.task_id.id], field_names, args, context=context)[issue.task_id.id]['progress']
-            res[issue.id] = {'progress' : progress}
-        return res
-
     def on_change_project(self, cr, uid, ids, project_id, context=None):
         if project_id:
             project = self.pool.get('project.project').browse(cr, uid, project_id, context=context)
@@ -216,20 +206,6 @@ class project_issue(osv.Model):
                 return {'value': {'partner_id': project.partner_id.id}}
         return {}
 
-    def _get_issue_task(self, cr, uid, ids, context=None):
-        issues = []
-        issue_pool = self.pool.get('project.issue')
-        for task in self.pool.get('project.task').browse(cr, uid, ids, context=context):
-            issues += issue_pool.search(cr, uid, [('task_id','=',task.id)])
-        return issues
-
-    def _get_issue_work(self, cr, uid, ids, context=None):
-        issues = []
-        issue_pool = self.pool.get('project.issue')
-        for work in self.pool.get('project.task.work').browse(cr, uid, ids, context=context):
-            if work.task_id:
-                issues += issue_pool.search(cr, uid, [('task_id','=',work.task_id.id)])
-        return issues
     _columns = {
         'id': fields.integer('ID', readonly=True),
         'name': fields.char('Issue', required=True),
@@ -284,12 +260,6 @@ class project_issue(osv.Model):
         'user_email': fields.related('user_id', 'email', type='char', string='User Email', readonly=True),
         'date_action_last': fields.datetime('Last Action', readonly=1),
         'date_action_next': fields.datetime('Next Action', readonly=1),
-        'progress': fields.function(_hours_get, string='Progress (%)', multi='hours', group_operator="avg", help="Computed as: Time Spent / Total Time.",
-            store = {
-                'project.issue': (lambda self, cr, uid, ids, c={}: ids, ['task_id'], 10),
-                'project.task': (_get_issue_task, ['progress'], 10),
-                'project.task.work': (_get_issue_work, ['hours'], 10),
-            }),
     }
 
     _defaults = {
